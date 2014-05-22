61153dcc639b1c773b817639ffcdc0218df9e39c
Peter Fealey <pete@Peters-MacBook-Pro.local>
2014-05-22 14:11:12 +0100

    add postcode

--- lib/custom_fields/types/post_code.rb
+++ lib/custom_fields/types/post_code.rb
@@ -26,14 +26,14 @@ module CustomFields
             klass.after_save do |object|
             	
               @base_google_url = "http://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address="
-    		  @addr = klass.post_code
+    		  @addr = self.post_code
             
     		  res = RestClient.get(URI.encode("#{@base_google_url}#{@addr}"))
     		  parsed_res = Crack::XML.parse(res)
               lat = parsed_res["GeocodeResponse"]["result"]["geometry"]["location"]["lat"]
               lng = parsed_res["GeocodeResponse"]["result"]["geometry"]["location"]["lng"]              
-              klass.send(:"#{lat}=", lat)
-              klass.send(:"#{lng}=", lng)
+              self.send(:"#{lat}=", lat)
+              self.send(:"#{lng}=", lng)
               
               
               
